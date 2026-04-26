1f3e32c2b40a7ad8f88912c5e9bb7ea6fd01abbc
tanikiti9 <shelly.ai800@gmail.com>
2026-04-26 22:07:17 +0900

    Add components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
+import Main from "@/components/main/Main";
+import SubHeader from "@/components/subHeader/SubHeader";
 
 export default function Home() {
   return (
     <div>
       <div className="header"><Header /></div>
+      <div className="header"><SubHeader /></div>
+      <div className="header"><Main /></div>
+      <div className="header"><Footer /></div>
     </div>
   );
 }
---
+++ frontend/src/components/main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from './sidebar/SideBar'
+import MainContainer from './main-container/MainContainer'
+
+const Main = () => {
+    return (
+        <>
+            <div><SideBar /></div>
+            <div><MainContainer /></div>
+        </>
+    )
+}
+
+export default Main
