ab3f2cbe41679c430af7551957a07343877c01fc
ocha_0524 <yingyaxiaotian@gmail.com>
2026-05-11 22:17:17 +0900

    cors

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,9 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->use([
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
