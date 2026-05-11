8032d7bcbf6a50b62b976e284d51c57aafc09bc4
ocha_0524 <yingyaxiaotian@gmail.com>
2026-05-11 21:59:44 +0900

    修正

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
